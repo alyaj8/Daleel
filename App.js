a3a8843a9b084f08fac2c2ef725da49b9e64442a
alyaj8 <99920164+alyaj8@users.noreply.github.com>
2022-11-30 13:56:09 +0300

    test

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View>   
+           <Text>Welcome to Daleel test </Text>
+     </View>
       <StatusBar style="auto" />
     </View>
   );
